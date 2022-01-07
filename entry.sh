54e4f29b84eb7d168132fb0144b8ec77f9392a61
Bartłomiej Tomala <bartek@gwi.com>
2022-01-07 22:00:43 +0100

    test python

--- entry.sh
+++ entry.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-kibot -c "$INPUT_CONFIG" -d "$INPUT_DIR" -b "$INPUT_BOARD" -e "$INPUT_SCHEMA"
+python src/kibot -c "$INPUT_CONFIG" -d "$INPUT_DIR" -b "$INPUT_BOARD" -e "$INPUT_SCHEMA"
+
+# -c Circuit/config.kibot.yml -d output -b 'Circuit/boneIO - relay board 24x10A/boneIO - relay board.kicad_pcb' -e 'Circuit/boneIO - relay board 24x10A/boneIO - relay board.sch'
