6b2da946068430b0241f449d08eccf0b807fa415
Bartłomiej Tomala <bartek@gwi.com>
2022-01-08 02:25:11 +0100

    clean up

--- entry.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python src/kibot -c "$INPUT_CONFIG" -d "$INPUT_DIR" -b "$INPUT_BOARD" -e "$INPUT_SCHEMA"
-
-# -c Circuit/config.kibot.yml -d output -b 'Circuit/boneIO - relay board 24x10A/boneIO - relay board.kicad_pcb' -e 'Circuit/boneIO - relay board 24x10A/boneIO - relay board.sch'
